ec7afcdf086d4f7fb4b35c74e2ccce92159626d2
tomascoelhocruz <tomascoelhocruz@gmail.com>
2021-06-08 20:29:52 +0100

    about section and portfolio section

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('a').click(function () {
+      $('a').removeClass("active");
+      $(this).addClass("active");
+    });
+  }); // section navbar active change
